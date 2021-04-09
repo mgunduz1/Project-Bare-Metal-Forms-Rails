9cc19bd79a965ec990ccf47858516c2f85651c6e
mgunduz1 <mgunduz114118@gmail.com>
2021-04-09 17:11:01 +0300

    add rubocop fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-
-  resources :users, only: [:new, :create, :edit, :update, :index]
-  root "users#index"
+  resources :users, only: %i[new create edit update index]
+  root 'users#index'
 end
